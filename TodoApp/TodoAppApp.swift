30f289d5d7bbe482e29045144a5f13eb89027576
Nitin Lodhi <nitinlodhi101@gmail.com>
2024-06-28 00:34:16 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by NITIN LODHI on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
